f416598ffb6f6a59bd371cd32e027c22614acca8
mechy88 <miguelandrerepatacodo@gmail.com>
2025-06-30 11:01:30 +0800

    Controllers setup

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { maximum: 20 }
+end
