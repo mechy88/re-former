64697e13bb1c80630cd8c9dcc65844b137339903
mechy88 <miguelandrerepatacodo@gmail.com>
2025-06-30 14:29:08 +0800

    Errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit # page
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update # post req update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      # @errors = @user.errors.full_messages
+      render :edit
+    end
+  end
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
@@ -13,7 +27,7 @@ class UsersController < ApplicationController
     elsif @user.save
       redirect_to new_user_path
     else
-      @errors = @user.errors.full_messages
+      @errors = User.find_by(username: @user.username).errors.full_messages
       render :new
     end
   end
